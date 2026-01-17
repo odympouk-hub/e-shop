4d4e893e007f1cc7519c238064a79970e4922ca7
odysseas <odympouk2000@gmail.com>
2026-01-17 15:57:12 +0200

    ALLAGES APLES

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { Products } from './pages/admin/products/products';
 import { Categories } from './pages/admin/categories/categories';
 import { Customer } from './pages/admin/customer/customer';
 import { Cart } from './pages/admin/cart/cart';
-import { TestComponent } from './pages/admin/test-component/test-component';
+
 
 export const routes: Routes = [
 
@@ -44,10 +44,9 @@ component:Categories
     path:'cart',
     component:Cart
 },
-{
-    path:'test-component',
-    component:TestComponent
-}
+
+ 
+
 ]
 
 },
--- src/app/pages/admin/login/login.ts
+++ src/app/pages/admin/login/login.ts
@@ -3,16 +3,19 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../../../services/user.service';
 import { User } from '../../../models/user';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+
+ 
   loginObj: any = {
 
     username: '',
@@ -35,4 +38,5 @@ export class Login {
   }
 
 
+ 
 }
